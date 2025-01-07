43e87d0dd4986bf0801a765f13b23815c50f0ce4
shintakino <joshuajaynarvaza@gmail.com>
2025-01-07 20:12:44 +0800

    Update: Fix Graph errors

--- anthracnose_monitor.py
+++ anthracnose_monitor.py
@@ -549,4 +549,4 @@ def retake_photo():
     return redirect(url_for('cam'))  # Redirect to the camera page
 
 if __name__ == '__main__':
-    app.run(debug=True, host='192.168.1.15', port=5000)
+    app.run(debug=True, host='192.168.0.15', port=5000)
